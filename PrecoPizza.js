f27361684d557473ff982d2853afb50cf5ecb865
stephanie possamai <stephaniepossamaiamanda@gmail.com>
2023-11-02 17:45:40 -0300

    calculo custo beneficio

--- PrecoPizza.js
+++ PrecoPizza.js
@@ -1,32 +1,61 @@
 
 
 
-let out = document.getElementById("idOut");
+var out = document.getElementById("idOut");
 
-let btInserir = document.getElementById("IdbtInserir");
-let btRelatorio = document.getElementById("idBtRelatorio");
-let contadorVezes=0;
-let pizzas = [];
+var btInserir = document.getElementById("IdbtInserir");
+var btRelatorio = document.getElementById("idBtRelatorio");
+var contadorVezes=0;
+var pizzas = [];
 
-btInserir.addEventListener("click", pegarDados)
+btInserir.addEventListener("click", pegarDados);
+btRelatorio.addEventListener("click", calcular);
 
 function pegarDados(){
-    let nome = document.getElementById("idNome");
-    let tamanho = document.getElementById("idTamanho");
-    let preco = document.getElementById("idPreco");
     contadorVezes++
-
-    // ver como o professor fez para puxar o nome direto sem o document.getElementById
+    let formulario = document.getElementById("idForm")
+    
     var pizza = {
-        nomeP: nome,
-        tamanhoP: tamanho,
-        precoP: preco
+        nome: formulario.nmNome.value,
+        tamanho: formulario.nmTamanho.value,
+        preco: formulario.nmPreco.value,
+        custoBeneficio: 0
     }
     pizzas.push(pizza);
-    console.table(pizza)
-    console.table(pizzas)
+    
+
 }
 
 function calcular() {
+    // transformar diametro em area 
+    for (let index = 0; index < pizzas.length; index++) {
+        pizzas[index].tamanho = (pizzas[index].tamanho)/2 * (pizzas[index].tamanho)/2 * 3.14; 
+    }
+    console.table(pizzas)
     
+
+    // calculando o custo beneficio
+    for (let index = 0; index < pizzas.length; index++) {
+        pizzas[index].custoBeneficio = (pizzas[index].preco) / (pizzas[index].tamanho); 
+    }
+    console.log("custo beneficio")
+    console.table(pizzas)
+    
+
+    //ordenar do melhor custo beneficio pro pior
+    function compare(a, b){
+            if (a.custoBeneficio > b.custoBeneficio) {
+                return 1
+            }
+            if (a.custoBeneficio < b.custoBeneficio) {
+                return -1
+            }
+            return 0 
+    }
+        console.table(+pizzas); 
+    pizzas.sort(compare)
+    console.table(pizzas); 
+
+
+
 }
