0a63bf27f76637b0fca7e42516b29b7dba900e5b
stephanie possamai <stephaniepossamaiamanda@gmail.com>
2023-11-05 21:49:20 -0300

    tratamento de repeticao do tamanho

--- PrecoPizza.js
+++ PrecoPizza.js
@@ -5,16 +5,23 @@ var out = document.getElementById("idOut");
 
 var btInserir = document.getElementById("IdbtInserir");
 var btRelatorio = document.getElementById("idBtRelatorio");
-var contadorVezes = 0;
 var pizzas = [];
 
 btInserir.addEventListener("click", pegarDados);
 btRelatorio.addEventListener("click", imprimir);
 
 function pegarDados() {
-    contadorVezes++
+    //document.getElementById("idOut").value = " ";
     let formulario = document.getElementById("idForm")
 
+    for (let index = 0; index < pizzas.length; index++) {
+        if (formulario.nmTamanho.value = pizzas[index].tamanho) {
+            // colocar um tempo de espera
+            document.getElementById("idOut").value = "Esse tamanho ja existe, tente novamente"
+            
+            pegarDados();
+        }
+    }
     var pizza = {
         nome: formulario.nmNome.value,
         tamanho: formulario.nmTamanho.value,
@@ -30,9 +37,9 @@ function pegarDados() {
 function calcular() {
     // transformar diametro em area 
     for (let index = 0; index < pizzas.length; index++) {
-        let calcTamanho = ((pizzas[index].tamanho)/2 * (pizzas[index].tamanho)/2) * 3.14;
+        let calcTamanho = ((pizzas[index].tamanho) / 2 * (pizzas[index].tamanho) / 2) * 3.14;
         pizzas[index].tamanho = calcTamanho.toFixed(0);
-        calcTamanho = 0; 
+        calcTamanho = 0;
     }
 
 
@@ -63,8 +70,8 @@ function calculoDiferenca() {
 
     for (let index = 1; index < pizzas.length; index++) {
         let diferenca = (((pizzas[index].custoBeneficio) / (pizzas[0].custoBeneficio) - 1) * 100);
-        pizzas[index].diferenca = "+"+ diferenca.toFixed(0) + "%";
-        diferenca=0;
+        pizzas[index].diferenca = "+" + diferenca.toFixed(0) + "%";
+        diferenca = 0;
     }
     console.table(pizzas)
 
@@ -81,25 +88,25 @@ function imprimir() {
     tr.innerHTML =
         "<th>Nome</th>" +
         "<th>Tam.(cm)</th>" +
-        "<th>Preço</th>" + 
-        "<th>R$ p/cm2</th>" + 
+        "<th>Preço</th>" +
+        "<th>R$ p/cm2</th>" +
         "<th>Diferença%</th>";
 
     thead.appendChild(tr);
 
 
-    pizzas.forEach(function(pizza) {
+    pizzas.forEach(function (pizza) {
         var tr = document.createElement('tr');
         tr.innerHTML =
-            "<td>"+pizza.nome+"</td>" +
-            "<td>"+pizza.tamanho+"</td>" +
-            "<td>"+pizza.preco+"</td>" + 
-            "<td> R$"+pizza.custoBeneficio+"</td>" + 
-            "<td>"+pizza.diferenca+"</td>";
+            "<td>" + pizza.nome + "</td>" +
+            "<td>" + pizza.tamanho + "</td>" +
+            "<td>" + pizza.preco + "</td>" +
+            "<td> R$" + pizza.custoBeneficio + "</td>" +
+            "<td>" + pizza.diferenca + "</td>";
 
         tbody.appendChild(tr);
 
     });
-    
+
 
 }
