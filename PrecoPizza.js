c0d7ea8301e52aee5cd265485dee03558d75dfba
stephanie possamai <stephaniepossamaiamanda@gmail.com>
2023-11-05 21:00:05 -0300

    imprimir tabela na tela

--- PrecoPizza.js
+++ PrecoPizza.js
@@ -5,57 +5,102 @@ var out = document.getElementById("idOut");
 
 var btInserir = document.getElementById("IdbtInserir");
 var btRelatorio = document.getElementById("idBtRelatorio");
-var contadorVezes=0;
+var contadorVezes = 0;
 var pizzas = [];
 
 btInserir.addEventListener("click", pegarDados);
-btRelatorio.addEventListener("click", calcular);
+btRelatorio.addEventListener("click", imprimir);
 
-function pegarDados(){
+function pegarDados() {
     contadorVezes++
     let formulario = document.getElementById("idForm")
-    
+
     var pizza = {
         nome: formulario.nmNome.value,
         tamanho: formulario.nmTamanho.value,
         preco: formulario.nmPreco.value,
-        custoBeneficio: 0
+        custoBeneficio: 0,
+        diferenca: 0
     }
     pizzas.push(pizza);
-    
+
 
 }
 
 function calcular() {
     // transformar diametro em area 
     for (let index = 0; index < pizzas.length; index++) {
-        pizzas[index].tamanho = (pizzas[index].tamanho)/2 * (pizzas[index].tamanho)/2 * 3.14; 
+        let calcTamanho = ((pizzas[index].tamanho)/2 * (pizzas[index].tamanho)/2) * 3.14;
+        pizzas[index].tamanho = calcTamanho.toFixed(0);
+        calcTamanho = 0; 
     }
-    console.table(pizzas)
-    
+
 
     // calculando o custo beneficio
     for (let index = 0; index < pizzas.length; index++) {
-        pizzas[index].custoBeneficio = (pizzas[index].preco) / (pizzas[index].tamanho); 
+        pizzas[index].custoBeneficio = ((pizzas[index].preco) / (pizzas[index].tamanho)).toFixed(2);
     }
-    console.log("custo beneficio")
-    console.table(pizzas)
-    
+
 
     //ordenar do melhor custo beneficio pro pior
-    function compare(a, b){
-            if (a.custoBeneficio > b.custoBeneficio) {
-                return 1
-            }
-            if (a.custoBeneficio < b.custoBeneficio) {
-                return -1
-            }
-            return 0 
+    function compare(a, b) {
+        if (a.custoBeneficio > b.custoBeneficio) {
+            return 1
+        }
+        if (a.custoBeneficio < b.custoBeneficio) {
+            return -1
+        }
+        return 0
     }
-        console.table(+pizzas); 
     pizzas.sort(compare)
-    console.table(pizzas); 
+
+    calculoDiferenca()
+}
+
+function calculoDiferenca() {
+    // calcular a diferença em % 
+    pizzas[0].diferenca = "Melhor CB";
+
+    for (let index = 1; index < pizzas.length; index++) {
+        let diferenca = (((pizzas[index].custoBeneficio) / (pizzas[0].custoBeneficio) - 1) * 100);
+        pizzas[index].diferenca = "+"+ diferenca.toFixed(0) + "%";
+        diferenca=0;
+    }
+    console.table(pizzas)
+
+}
+
+function imprimir() {
+    calcular();
+
+    var table = document.getElementById("idTable")
+    var thead = document.getElementById("idThead")
+    var tbody = document.getElementById("idTbody")
+
+    
+    var tr = document.createElement('tr');
+    tr.innerHTML =
+        "<th>Nome</th>" +
+        "<th>Tam.(cm)</th>" +
+        "<th>Preço</th>" + 
+        "<th>R$ p/cm2</th>" + 
+        "<th>Diferença%</th>";
+
+    thead.appendChild(tr);
 
 
+    pizzas.forEach(function(pizza) {
+        var tr = document.createElement('tr');
+        tr.innerHTML =
+            "<td>"+pizza.nome+"</td>" +
+            "<td>"+pizza.tamanho+"</td>" +
+            "<td>"+pizza.preco+"</td>" + 
+            "<td> R$"+pizza.custoBeneficio+"</td>" + 
+            "<td>"+pizza.diferenca+"</td>";
+
+        tbody.appendChild(tr);
+
+    });
+    
 
 }
